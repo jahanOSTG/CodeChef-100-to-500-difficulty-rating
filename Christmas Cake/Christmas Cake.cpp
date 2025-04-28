a7af625f4e3d7ec0224f8d727356e412c8f6ad28
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-28 09:05:19 +0600

    Add files via upload

---
+++ Christmas Cake/Christmas Cake.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    int count=0;
+    for(int i=x; i<=24; i++){
+            count++;
+
+    }
+    cout<<count<<endl;
+
+}
