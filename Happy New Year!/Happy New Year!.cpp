6a60aabcca093f1b27753bf0a17c295350759fe6
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-28 11:45:39 +0600

    Add files via upload

---
+++ Happy New Year!/Happy New Year!.cpp
@@ -0,0 +1,13 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    int count=0;
+    cin>>x;
+    for(int i=x; i<24;i++){
+            count++;
+
+    }
+    cout<<count<<endl;
+}
