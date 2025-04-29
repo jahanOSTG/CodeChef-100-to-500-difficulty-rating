17e9c0c0bd618d5d6a47df720e15e4a586c1e412
Faria Jahan Janie <janiefaria315@gmail.com>
2025-04-29 23:08:31 +0600

    Add files via upload

---
+++ Reach on Time/Reach on Time.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int t,x;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        cin>>x;
+        if(x<30){
+            cout<<"NO"<<endl;
+        }else{
+            cout<<"YES"<<endl;
+        }
+    }
+
+}
