625c4a32af3ed6f63516c026a3479e6e8100ab67
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-01 23:09:02 +0600

    Add files via upload

---
+++ Sum it/Sum it.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t,x,y,z;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>x>>y>>z;
+        if((x+y)==z){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+
+    }
+}
