059d4c30ebbead9b84d4c9fbdd7edf37ba6b22a1
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-01 22:23:43 +0600

    Add files via upload

---
+++ Chef and Donation/Chef and Donation.cpp
@@ -0,0 +1,17 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y;
+	int z;
+	cin>>t;
+	for(int i=0;i<t;i++){
+	    cin>>x>>y;
+	    z=abs(x-y);
+	    cout<<z<<endl;
+
+	}
+
+}
