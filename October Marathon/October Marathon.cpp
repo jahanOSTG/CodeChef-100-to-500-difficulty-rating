40e03cf81a07e49ee0aa4f353bf4af07778ad284
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-02 16:34:22 +0600

    Add files via upload

---
+++ October Marathon/October Marathon.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    if(x<3){
+        cout<<"GOLD"<<endl;
+    }else if(x>=6){
+        cout<<"BRONZE"<<endl;
+
+    }else{
+        cout<<"SILVER"<<endl;
+
+    }
+}
