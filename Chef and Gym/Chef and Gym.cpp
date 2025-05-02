505585cded78e6f55579150c65f319aa8ba5b721
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-02 22:01:12 +0600

    Add files via upload

---
+++ Chef and Gym/Chef and Gym.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+int main() {
+    int t, x,y,z;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        cin >>x>>y>>z;
+        if ((x+y)<=z) {
+            cout << 2 << endl;
+        }else if(x>z ){
+            cout <<0 << endl;
+        } else {
+            cout <<1 << endl;
+        }
+    }
+}
