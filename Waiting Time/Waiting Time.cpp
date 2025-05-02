ed1c6b04b572074e7db54d1ba7a8d3aa33a787c3
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-02 08:36:58 +0600

    Add files via upload

---
+++ Waiting Time/Waiting Time.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int t,x,y;
+    int a;
+    int b;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>x>>y;
+        a=x*7;
+        b=a-y;
+        cout<<b<<endl;
+
+    }
+}
