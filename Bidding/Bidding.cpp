ea32de8c849990fefded1d4a9eaf303456f38fa8
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-03 15:34:17 +0600

    Add files via upload

---
+++ Bidding/Bidding.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t,a,b,c;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>a>>b>>c;
+        if(a>b && a>c){
+            cout<<"Alice"<<endl;
+        }else if(b>c && b>a){
+            cout<<"Bob"<<endl;
+        }else{
+            cout<<"Charlie"<<endl;
+        }
+    }
+}
