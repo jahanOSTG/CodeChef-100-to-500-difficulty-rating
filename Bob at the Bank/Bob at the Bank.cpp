bb1206b58e168dd3d44c3ebbf6a254145cb2da16
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-05 10:13:49 +0600

    Add files via upload

---
+++ Bob at the Bank/Bob at the Bank.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t,x,y,z,s;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>x>>y>>z>>s;
+       cout<<x+(y-z)*s<<endl;
+
+  }
+}
+
