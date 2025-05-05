d895dda3efeecb33a7229d7c5e5cbf98d5181515
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-05 09:10:39 +0600

    Add files via upload

---
+++ Lunchtime/Lunchtime.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t,x;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>x;
+        if(x>=1 && x<=4){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+
+  }
+}
