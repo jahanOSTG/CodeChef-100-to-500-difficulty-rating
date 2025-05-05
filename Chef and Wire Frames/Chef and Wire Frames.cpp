6f71113b135b1dc5c675c2b5b9384a849b4ac6db
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-05 23:19:06 +0600

    Add files via upload

---
+++ Chef and Wire Frames/Chef and Wire Frames.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t,a,b,c,d;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>a>>b>>c;
+        d=2*a+2*b;
+        cout<<d*c<<endl;
+    }
+}
