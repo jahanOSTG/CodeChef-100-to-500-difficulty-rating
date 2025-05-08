d533e634e3df3772acd3976f6b987f042adfe74c
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-08 23:07:52 +0600

    Add files via upload

---
+++ Bucket and Water Flow/Bucket and Water Flow.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    int t,a,b,c,d,e;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>a>>b>>c>>d;
+        e=a+c*d;
+        if(e>b){
+            cout<<"overflow"<<endl;
+        }else if(e==b){
+             cout<<"filled"<<endl;
+            }else{
+            cout<<"Unfilled"<<endl;
+        }
+
+    }
+}
