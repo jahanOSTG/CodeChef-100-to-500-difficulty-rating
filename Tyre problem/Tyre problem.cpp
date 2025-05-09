a6b5266c5ce56a9a8c3a14c98df401fe80841c41
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-09 23:13:38 +0600

    Add files via upload

---
+++ Tyre problem/Tyre problem.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    int t,a,b;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>a>>b;
+        cout<<a*2+b*4<<endl;
+
+
+    }
+}
