8f17d5c7ef0ea2d19cbf0c9b6f9a3b07c149e76c
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-10 23:36:53 +0600

    Add files via upload

---
+++ Practice makes us perfect/Practice makes us perfect.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a,b,c,d;
+    int count=0;
+
+    cin>>a>>b>>c>>d;
+    if(a>=10){
+        count=count+1;
+    }if(b>=10){
+         count=count+1;
+    }if(c>=10){
+         count=count+1;
+    }if(d>=10){
+         count=count+1;
+    }
+    cout<<count<<endl;
+
+
+
+
+
+
+}
+
