06fe524c477fc7e2a1aa480de75b8851eac83d77
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-12 10:09:47 +0600

    Add files via upload

--- Course Registration/Course Registration.cpp
+++ Course Registration/Course Registration.cpp
@@ -1,14 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int t,a,b,c;
+      int t,a,b,c,d;
     cin>>t;
-    for(int i=0; i<t;i++){
+    for(int i=0;i<t;i++){
         cin>>a>>b>>c;
-        if((b-c)<a){
-            cout<<"NO"<<endl;
+        d=b-c;
+        if(a<=d){
+            cout<<"YES"<<endl;
         }else{
-             cout<<"YES"<<endl;
+            cout<<"NO"<<endl;
         }
     }
 }
