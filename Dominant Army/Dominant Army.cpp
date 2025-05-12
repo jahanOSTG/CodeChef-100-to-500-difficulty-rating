08649b61888639a367be8f0689ee1be40b107d12
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-12 10:20:02 +0600

    Add files via upload

---
+++ Dominant Army/Dominant Army.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int t,a,b,c,d;
+    int sum,n_max;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>a>>b>>c;
+        sum=a+b+c;
+        n_max=max({a,b,c});
+        d=sum-n_max;
+        if(n_max>d){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+
+
+
+    }
+}
