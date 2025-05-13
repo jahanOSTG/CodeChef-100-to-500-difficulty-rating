3713816b5aaca681e7d231db4585d1c50b6a4b68
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-13 22:37:04 +0600

    Add files via upload

---
+++ Playlist/Playlist.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t,a,b,c;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>a>>b;
+        c=b*3;
+        cout<<a/c<<endl;
+
+    }
+}
