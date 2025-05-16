4c154381a5b1d1a2347b5fcaa4b02199b0b5e160
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-16 10:54:53 +0600

    Add files via upload

---
+++ Codechef Airlines/Codechef Airlines.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int t,a,b,c,d;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>a>>b>>c;
+        d=a*10;
+        if(d<=b){
+            cout<<d*c<<endl;
+        }else{
+            cout<<b*c<<endl;
+        }
+
+    }
+}
