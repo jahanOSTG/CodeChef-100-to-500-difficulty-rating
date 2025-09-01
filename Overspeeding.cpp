5fbbbeb5d7493b506e304133c10de8d5a0a65ed0
Faria Jahan Janie <janiefaria315@gmail.com>
2025-09-01 21:02:49 +0600

    Create Overspeeding.cpp

---
+++ Overspeeding.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a;
+	cin>>a;
+	if(a<=40){
+	    cout<<"NO"<<endl;
+	}else{
+	    cout<<"YES"<<endl;
+	} 
+
+}
