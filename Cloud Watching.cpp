cd4fdf26a2738a3f953878809ce71c36f3d35f3c
Faria Jahan Janie <janiefaria315@gmail.com>
2025-09-20 23:40:06 +0600

    Create Cloud Watching.cpp

---
+++ Cloud Watching.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin>>a>>b;
+	if(a*3<=b){
+	    cout<<"Rain"<<endl;
+	}else{
+	    cout<<"DRY"<<endl;
+	}
+
+}
