c2aab3ddcad701b985567005e01992548378ff72
Faria Jahan Janie <janiefaria315@gmail.com>
2025-11-24 22:32:13 +0600

    Create Rating Improvement.cpp

---
+++ Rating Improvement.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	int a,b;
+	while(t--){
+	    cin>>a>>b;
+	    if(b>=a && b<=(a+200)){
+	        cout<<"YES"<<endl;
+	        
+	    }else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	
+
+}
