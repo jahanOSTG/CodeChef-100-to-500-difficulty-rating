fe04a1849ab88633b3ce807cae13231a390c8ba6
Faria Jahan Janie <janiefaria315@gmail.com>
2025-11-28 23:33:26 +0600

    Create Rectangle and Square.cpp

---
+++ Rectangle and Square.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,c;
+	cin>>a>>b>>c;
+	int rec=a*b;
+	int sqr=c*c;
+	if(rec==sqr) cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
+
+}
