9163ae51234662c39320191bb257a7fff16c8428
Faria Jahan Janie <janiefaria315@gmail.com>
2025-11-30 23:26:39 +0600

    Update Bidding.cpp

--- Bidding/Bidding.cpp
+++ Bidding/Bidding.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 using namespace std;
-int main(){
-    
+int main(){ 
     int t,a,b,c;
     cin>>t;
     for(int i=0;i<t;i++){
@@ -15,4 +14,5 @@ int main(){
         }
     }
 }
+
 
